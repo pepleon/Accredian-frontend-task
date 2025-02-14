a55e8706f1ff47720ac7852f09294ada7a6eb417
Aman Butt <156330345+pepleon@users.noreply.github.com>
2025-02-14 19:38:48 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 
 
 export const BASE_URL = 
-location.hostname === "localhost" ? "http://localhost:3000/" : "https://skibidiref-1.onrender.com/";
+location.hostname === "localhost" ? "http://localhost:3000/" : "https://skibidiref-2.onrender.com/";
